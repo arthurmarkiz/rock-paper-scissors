ab636d75536755d26362e5a2afe2e91fbfbc9124
Arthur Markiz <tutumarkiz26@gmail.com>
2024-09-16 11:47:38 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+
+  return (
+    <>
+    <div className="bg-slate-800">
+      <h1 className="text-3xl font-bold text-emerald-500">Hello World!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
