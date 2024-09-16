dd6e2f9de9e98d30339f3fe4926b7ab8d6af38a0
Arthur Markiz <tutumarkiz26@gmail.com>
2024-09-16 17:02:02 -0300

    :sparkles: Add Header initial design and SVGs

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+const Header = () => {
+    return (
+        <>
+        <header className="pt-8 flex flex-col items-center text-black mb-8">
+            <h1 className="text-5xl font-extrabold mb-4">Rock, Paper, Scissors</h1>
+            <p className="text-sm">It's a great game to have fun and pass the time.</p>
+            <p className="text-sm">Choose your move and <span className="font-semibold text-softGreen">Good Luck!</span></p>
+        </header>
+        </>
+    )
+}
+
+export default Header
