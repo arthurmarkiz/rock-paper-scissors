02641a15d72bd663fbfa3428c798bd5a99a3d724
Arthur Markiz <tutumarkiz26@gmail.com>
2024-09-16 21:07:54 -0300

    :sparkles: Add core game functionality and reset option

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,22 +2,116 @@ import RockHand from "../public/rock-hand.svg"
 import PaperHand from "../public/paper-hand.svg"
 import ScissorsHand from "../public/scissors-hand.svg"
 import Result from "./Result.jsx"
+import { useState } from "react"
 
 
 const Game = () => {
+    const [userChoice, setUserChoice] = useState()
+    const [pcChoice, setPcChoice] = useState()
+    const [result, setResult] = useState()
+
+    const [userScore, setUserScore] = useState(0)
+    const [pcScore, setPcScore] = useState(0)
+
+    const [gameStarted, setGameStarted] = useState(false)
+
+    const options = ['Rock', 'Paper', 'Scissors']
+
+    const determineWinner = (user, pc) => {
+        if (user === pc)
+            return 'Draw'
+
+        if (
+            (user === 'Rock' && pc === 'Scissors') ||
+            (user === 'Scissors' && pc === 'Paper') ||
+            (user === 'Paper' && pc === 'Rock')
+        ) {
+            setUserScore(prev => prev + 1)
+            return 'Win'
+        }
+        else {
+            setPcScore(prev=>prev+1)
+            return 'Lost'
+        }
+    }
+
+    const handleClick = (choice) => {
+        setUserChoice(choice)
+        const pc = options[Math.floor(Math.random() * options.length)]
+        setPcChoice(pc)
+
+        const gameResult = determineWinner(choice, pc)
+        setResult(gameResult)
+
+        setGameStarted(true)
+        }
+    
+
+    const handleReset = () => {
+        setUserChoice('')
+        setPcChoice('')
+        setUserScore(0)
+        setPcScore(0)
+        setResult('')
+    }
+
+    if (userScore >= 10) {
+        handleReset()
+    }
+    if (pcScore >= 10) {
+        handleReset()
+    }
+
+    let fillBg
+    if (!gameStarted)
+        fillBg = 'mb-[270px]'
+    if (gameStarted)
+        fillBg =  ''
+
     return (
         <>
-        <div className="flex space-x-36 justify-center items-center mt-6 mb-12 mr-5">
-            <button className="hover:scale-105 duration-300"><img className="h-36 mt-12" src={RockHand} /></button>
-            <button className="hover:scale-105 duration-300"><img className="h-56" src={PaperHand} /></button>
-            <button className="hover:scale-105 duration-300"><img className="h-44 mt-6" src={ScissorsHand} /></button>
+        <div className={"flex space-x-36 justify-center items-center mt-6 mb-12 mr-5 " + fillBg}>
+            <button 
+                key='Rock'
+                className="hover:scale-105 duration-300"
+                onClick={()=>handleClick('Rock')}    
+            >
+                <img className="h-36 mt-12" src={RockHand} />
+            </button>
+            <button 
+                key='Paper'
+                className="hover:scale-105 duration-300"
+                onClick={()=>handleClick('Paper')} 
+            >
+                <img className="h-56" src={PaperHand} />
+            </button>
+            <button
+                key='Scissors'
+                className="hover:scale-105 duration-300"
+                onClick={()=>handleClick('Scissors')}    
+            >
+                <img className="h-44 mt-6" src={ScissorsHand} />
+            </button>
         </div>
-        <div className="border-b border-gray w-full"></div>
-        <Result />
-        <div>
-            <button className="bg-softYellow px-8 py-2 mt-8 mb-10 rounded-md font-bold tracking-wide hover:scale-105 hover:brightness-110 duration-300">Reset</button>
-        </div>
-        <div className="border-b border-gray w-full"></div>
+        {gameStarted && <>
+            <div className="flex space-x-24 mb-8 justify-center text-black">
+                <p>👼 Your Choice: <span className="font-bold ml-3">{userChoice}</span></p>
+                <p className="font-extrabold">VS</p>
+                <p>💻 PC Choice: <span className="font-bold ml-3">{pcChoice}</span></p>
+            </div>
+            <Result result={result} userScore={userScore} pcScore={pcScore}/>
+            <div>
+                <button 
+                    className="bg-softYellow px-8 py-2 mt-8 mb-10 rounded-md font-bold tracking-wide 
+                    hover:scale-105 hover:brightness-110 duration-300"
+                    onClick={handleReset}
+                    >
+                        Reset
+                    </button>
+            </div>
+            <div className="border-b border-gray w-full"></div>
+            </>
+        }
         </>
     )
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,21 +1,43 @@
 
 
-const Result = () => {
+const Result = ({result, userScore, pcScore}) => {
+
+    let resultText
+    let resultColor
+
+    if (result === 'Win') {
+        resultText = 'You Win!'
+        resultColor = 'text-softGreen'
+    }
+    if (result === 'Lost') {
+        resultText = 'You Lost!'
+        resultColor = 'text-softRed'
+    }
+    if (result === 'Draw') {
+        resultText = "Draw!"
+        resultColor = 'text-softYellow'
+    }
+
     return (
         <>
+        <div className="border-b border-gray w-full"></div>
         <div className="flex flex-col justify-center items-center mt-10">
             <div>
-                <h3 className="text-5xl font-bold text-softGreen">You Win!</h3>
+                <h3 className={'text-5xl font-bold ' + resultColor}>
+                    {resultText}
+                </h3>
             </div>
-            <div className="flex flex-col justify-center items-center bg-softRed text-white p-6 border-2 border-white rounded-lg shadow-lg shadow-black/40 mt-10 mb-6 px-14">
+            <div 
+            className="flex flex-col justify-center items-center bg-softRed text-white p-6 border-2 border-white rounded-lg 
+            shadow-lg shadow-black/40 mt-5 mb-6 px-14 w-[310px]">
                 <label className="text-4xl font-bold tracking-wide">SCORE</label>
                 <div className="text-7xl my-4 flex space-x-10 font-bold items-center">
-                    <label>3</label>
+                    <label>{userScore}</label>
                     <label className="text-4xl font-light text-softYellow"> X </label>
-                    <label>2</label>
+                    <label>{pcScore}</label>
                 </div>
                 <div className="flex w-full justify-between font-semibold">
-                    <p className="-ml-1">Player</p>
+                    <p className="">Player</p>
                     <p className="mr-3">PC</p>
                 </div>
             </div>
